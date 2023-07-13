52091d4fbdcb6098538cdd8adece7f80444ad32a
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2023-07-13 09:22:19 +0800

    working on controller

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,19 +1,26 @@
 const asyncHandler = require('express-async-handler');
 const express = require('express');
 const {default : mongoose} = require('mongoose');
+const escrowContract = require('../models/escrowContract');
 
 //@desc Get escrow contracts
 //@route GET /api/escrow
 //@access Public
 const getEscrowContracts = asyncHandler(async (req, res) => {
-
+    const escrowContracts = await escrowContract.find({});
+    if (!escrowContracts) {
+        res.status(404);
+        console.log('No escrow contracts found');
+        throw new Error('No escrow contracts found');
+    } else {
+        res.status(200).json(escrowContracts);
+    }
 });
 
 //@desc Get escrow contract by contract address
 //@route GET /api/escrow/:address
 //@access Public
 const getEscrowContract = asyncHandler(async (req, res) => {
-
 });
 
 //@desc Create escrow contract
---
+++ server/models/escrowContract.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const escrowContractSchema = mongoose.Schema({
+    contractAddress: {
+        type: String,
+        required: true,
+    },
+    depositor: {
+        type: String,
+        required: true,
+    },
+    beneficiary: {
+        type: String,
+        required: true,
+    },
+    arbiter: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    network: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    }
+});
+
+module.exports = mongoose.model('escrowContract', escrowContractSchema, 'escrowContract');
